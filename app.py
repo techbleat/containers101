5f9c44fe4405847e2494f6ac29341b85bc6d78b8
shegoj <shegoj@yahoo.com>
2023-07-16 21:52:42 +0100

    added deploy stage

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-regions = {"uk":"London","nigeria":"Lagos","southafrica":"Johannesburg", "usa":"DC", "us":"DC","Ghana":"accra"}
-version = "1.5"
+regions = {"uk":"London","nigeria":"Lagos","southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
+version = "1.6"
 
 @app.route("/getregion/<country>")
 def lookup(country):
     value ="N/A"
     try:
-        value = regions [country]
+        value = regions [country.lower()]
     except:
         pass
     return value 
